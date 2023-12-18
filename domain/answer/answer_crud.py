5b0c1b28166a2674feb7e6156a1e34d94af74e39
Jung Yong-Han <dydgks789@naver.com>
2023-12-18 22:43:07 +0900

    test

--- domain/answer/answer_crud.py
+++ domain/answer/answer_crud.py
@@ -3,7 +3,6 @@ from datetime import datetime
 from domain.answer.answer_schema import AnswerCreate
 from models import Question, Answer
 
-
 def create_answer(question: Question, answer_create: AnswerCreate):
     new_answer = Answer(
         content=answer_create.content,
--- models.py
+++ models.py
@@ -1,20 +1,42 @@
-from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey
-from sqlalchemy.orm import relationship
-from database import Base
+from mongoengine import (Document, StringField, DateTimeField, ListField,
+                         EmbeddedDocument, EmbeddedDocumentField)
 
-class Question(Base):
-    __tablename__ = "question"
+# Embed 사용 - 하나의 문서 안에 다른 문서를 중첩시켜 저장할 수 있다.
+# 내장 문서는 부모 문서에 종속되므로 부모 문서가 삭제되면 내장 문서도 함께 삭제된다.
+# 게시판 목적에 더 적합하다고 판단
 
-    id = Column(Integer, primary_key=True)
-    subject = Column(String, nullable=False)
-    content = Column(Text, nullable=False)
-    create_date = Column(DateTime, nullable=False)
+class Answer(EmbeddedDocument):
 
-class Answer(Base):
-    __tablename__ = "answer"
+    #author = StringField(required=True)
+    content = StringField(required=True)
+    create_date = DateTimeField(required=True)
 
-    id = Column(Integer, primary_key=True)
-    content = Column(Text, nullable=False)
-    create_date = Column(DateTime, nullable=False)
-    question_id = Column(Integer, ForeignKey("question.id"))
-    question = relationship("Question", backref="answers")
+class Question(Document):
+
+    #author = StringField(required=True)
+    subject = StringField(required=True)
+    content = StringField(required=True)
+    create_date = DateTimeField(required=True)
+    answers = ListField(EmbeddedDocumentField(Answer))
+
+# 참조 사용
+"""
+
+class Question(Document):
+    subject = StringField(required=True)
+    content = StringField(required=True)
+    create_date = DateTimeField(required=True)
+
+class Answer(Document):
+    content = StringField(required=True)
+    create_date = DateTimeField(required=True)
+    question = ReferenceField(Question)
+
+# Create a new question
+new_question = Question(subject="Example Subject", content="Example Content", create_date="2023-01-01T12:00:00")
+new_question.save()
+
+# Create a new answer referencing the question by its ObjectId
+new_answer = Answer(content="Example Answer Content", create_date="2023-01-02T14:00:00", question=new_question)
+new_answer.save()
+"""
